e7012d1ffecc18ba04f53d8c730f8564fed367a9
Polks <p.marinelli2@studenti.unipi.it>
2024-01-27 11:06:14 +0100

    Ordinato leggermente il codice

--- src/my_package/scripts/waypoint_with_service.py
+++ src/my_package/scripts/waypoint_with_service.py
@@ -6,11 +6,14 @@ from    my_package.srv      import PathLength, PathLengthResponse
 
 #  Callback che fa partire l'esecuzione del nodo
 def callback(msg):
+    
     global enable
+
     enable = True
 
 #  Funzione per calcoalre la distanza euclidea tra 2 punti
 def distance_calculator(P1, P2):
+
     x1 = P1[0]
     y1 = P1[1]
 
@@ -18,12 +21,14 @@ def distance_calculator(P1, P2):
     y2 = P2[1]
 
     distance = math.pow(math.pow(x1 - x2, 2) + math.pow(y1 - y2, 2), 0.5)
+
     return distance
 
 # Service server presente in ogni nodo:
 # Quando viene ricevuta una richiesta contenente l'id di un waypoint
 # restituisce la distanza del robot da quel punto
 def handle_path_distance_request(request):
+
     global waypoints
     global robot_position
 
@@ -33,12 +38,13 @@ def handle_path_distance_request(request):
     return response
 
 def waypoint_assignment():
+
     global enable
     global robot_position
     global waypoints
     global my_id
-    
-    enable = False
+
+    # Init del nodo
     rospy.init_node("waypoint_assignment")
     rate = rospy.Rate(10)
 
@@ -67,12 +73,12 @@ def waypoint_assignment():
             other_id.append(id)   
 
     # Topic per debug
-    sub = rospy.Subscriber("/chat", String, callback, queue_size=1)
-    pub_test = rospy.Publisher("test", Float64, queue_size=2)
-    pub = rospy.Publisher("waypoint_assigned", Int8, queue_size=1)
+    sub = rospy.Subscriber("/chat",             String,     callback, queue_size=1)
+    pub_test = rospy.Publisher("test",          Float64,    queue_size=2)
+    pub = rospy.Publisher("waypoint_assigned",  Int8,       queue_size=1)
 
     # Init del service server
-    my_service = rospy.Service("path_length", PathLength, handle_path_distance_request)
+    my_server = rospy.Service("path_length", PathLength, handle_path_distance_request)
      
     # Init service client, uno per ogni altro agente
     for i in range(len(other_id)):
@@ -80,12 +86,16 @@ def waypoint_assignment():
         rospy.wait_for_service(service_name)
         service_call[other_id[i]] = rospy.ServiceProxy(service_name, PathLength)
 
-    test_msg = Float64()
-    result_msg = Int8()
+    # Init messaggi da pubblicare
+    test_msg    = Float64()
+    result_msg  = Int8()
     
+    # Valori iniziali variabli ancora non assegnate
     robot_position = [x_0, y_0]
-
+    enable = False
+    
     while not rospy.is_shutdown():
+
         n_waypoints = len(waypoints)
 
         if enable is True:
@@ -101,15 +111,17 @@ def waypoint_assignment():
             waypoint_orderd = sorted(waypoints_distance.items(), key=lambda item:item[1])
 
             # Id del waypoint con la distanza minore 
-            candidate = int(waypoint_orderd[0][0])
-            my_distance = waypoint_orderd[0][1]
+            my_candidate   = int(waypoint_orderd[0][0])
+            my_distance    = waypoint_orderd[0][1]
 
-            # Chiamata al server di tutti gli altri agenti per ottenere la loro distanza dal mio 
-            # waypoint candidato
+            # Chiamata al server di tutti gli altri agenti per ottenere la loro distanza da
+            # my_candidate
             for i in range(len(other_id)):
                 # Richiedo la distanza a other_id[i]
-                other_dist = service_call[other_id[i]](candidate).distance
-                # Salvo la distanza nel dizionario
+                other_dist = service_call[other_id[i]](my_candidate).distance
+                # Salvo la distanza nel dizionario SERVE VERAMENTE UN DIZIONARIO ANCHE QUI?
+                # IN TEORIA NO, MI BASTA CONOSCERE LE DISTANZE DEGLI ALTRI ROBOT, SENZA SAPERE 
+                # I LORO ID.
                 other_robot_distance[other_id[i]] = other_dist
                 test_msg.data = other_dist
                 pub_test.publish(test_msg)

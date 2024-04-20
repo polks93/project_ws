b30f4ac1a5272c4671a5e0761a548492414439c8
Polks <p.marinelli2@studenti.unipi.it>
2024-04-20 20:09:35 +0200

    Changed_cells_union funzionante su due robot

--- src/map-change-detection/scripts/changed_cells_union.py
+++ src/map-change-detection/scripts/changed_cells_union.py
@@ -5,16 +5,21 @@ from    map_change_detection.msg import ChangedCells
 
 def callback(msg,arg):
     global callback_recived
-    
+    global OccBuffer
+    global FreeBuffer
+
+    robot_id = arg
     toFree = list(msg.toFree)
     toOcc = list(msg.toOcc)
 
-    robot_id = arg
-
-    callback_recived[robot_id] = [True, toFree, toOcc]
+    OccBuffer = list(set(OccBuffer) | set(toOcc))
+    FreeBuffer = list(set(FreeBuffer) | set(toFree))
+    callback_recived[robot_id] = True
 
 def changed_cells_union():
     global callback_recived
+    global FreeBuffer
+    global OccBuffer
 
     rospy.init_node("changed_cells_union")
     rate = rospy.Rate(20)
@@ -35,29 +40,28 @@ def changed_cells_union():
         robot_id.append(i+1)
 
     for i in range(len(robot_id)):
-        subscribers[robot_id[i]] = rospy.Subscriber(cells_topic_name[i], ChangedCells, callback, queue_size=2, callback_args=robot_id[i])
-        callback_recived[robot_id[i]] = [False, 0, 0]
+        subscribers[robot_id[i]] = rospy.Subscriber(cells_topic_name[i], ChangedCells, callback, queue_size=1, callback_args=robot_id[i])
+        callback_recived[robot_id[i]] = False
 
     pub = rospy.Publisher("total_changed_cells", ChangedCells, queue_size=1)
     total_changed_cells = ChangedCells()
     while not rospy.is_shutdown():
 
         for i in range(len(robot_id)):
-            if callback_recived[robot_id[i]][0] is False:
+            if callback_recived[robot_id[i]] is False:
                 break
 
             elif i == n_robot - 1:
                 
-            # for i in range(len(robot_id)):
-                # FreeBuffer.append(callback_recived[robot_id[i]][1])
-                # OccBuffer.append(callback_recived[robot_id[i]][2])
-                OccBuffer = callback_recived[robot_id[0]][2] + callback_recived[robot_id[1]][2]
-                rospy.loginfo(OccBuffer)
-                total_changed_cells.toOcc = OccBuffer
-                callback_recived[robot_id[i]] = [False, 0, 0]
-                
+                for i in range(len(robot_id)):
+                    callback_recived[robot_id[i]] = False
+
+                total_changed_cells.toFree = FreeBuffer
+                total_changed_cells.toOcc = OccBuffer 
                 pub.publish(total_changed_cells)
                 total_changed_cells = ChangedCells()
+                FreeBuffer = []
+                OccBuffer = []
 
         rate.sleep()
 
